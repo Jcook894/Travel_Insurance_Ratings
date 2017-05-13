a21f88a09426e9b1da1d833fcadfb816838ff347
Jcook894 <jcook894@icloud.com>
2017-05-13 12:48:36 -0400

    Feat: Customizing the logo text in the navbar.

--- t3-assets/dev/t3/d1e25eb0d39d871607bb0256a6990094-cache-t3-22787efba03f3bf3109a5d0f1ce499c0.php
+++ t3-assets/dev/t3/d1e25eb0d39d871607bb0256a6990094-cache-t3-22787efba03f3bf3109a5d0f1ce499c0.php
@@ -1,1 +1,1 @@
-<?php die("Access Denied"); ?>#x#i:1494642402;
+<?php die("Access Denied"); ?>#x#i:1494693962;
--- t3-assets/dev/t3/d1e25eb0d39d871607bb0256a6990094-cache-t3-2367cc2ebfb3f382a0fe44fdb7d56563.php
+++ t3-assets/dev/t3/d1e25eb0d39d871607bb0256a6990094-cache-t3-2367cc2ebfb3f382a0fe44fdb7d56563.php
@@ -1,1 +1,1 @@
-<?php die("Access Denied"); ?>#x#i:1494642412;
+<?php die("Access Denied"); ?>#x#i:1494693976;
